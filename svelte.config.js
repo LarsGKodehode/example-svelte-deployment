90559d0a700d2d7988b5a6b63535052bd9b671ba
LarsG <lars.ga.solheim@gmail.com>
2023-01-28 16:16:17 +0100

    removed pages with non prerenderable content
    somthing to be mindefull of when deploying to various hosting providers

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-github';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,16 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null,
+      precompress: false,
+      domain: '',
+      jekyll: false
+    })
 	}
 };
 
