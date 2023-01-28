4a2e1483564403721116a6d6783b5cb042e0e0db
LarsG <lars.ga.solheim@gmail.com>
2023-01-28 16:47:51 +0100

    Updates

---
+++ _app/immutable/chunks/1-2b88d61c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-17cea89e.js";export{t as component};
---
+++ _app/immutable/chunks/stores-7363e874.js
@@ -0,0 +1,1 @@
+import"./index-4f507e4d.js";import{s as e}from"./singletons-4f4a66af.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
